b613e2f4576d3a2b4433fe8952971f5c1fe412fb
Aayush Vaibhaw <127970657+aayushvbw@users.noreply.github.com>
2025-02-25 11:25:50 +0530

    error kind of

--- app/api/servers/[serverId]/leave/route.ts
+++ app/api/servers/[serverId]/leave/route.ts
@@ -4,7 +4,6 @@ import { NextResponse } from "next/server";
 
 
 
-
 export async function PATCH(
     req: Request,
     { params }: { params: { serverId: string } }
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -65,7 +65,7 @@ export const ServerHeader = ({
                 )}
 
                 {isAdmin && (
-                    <DropdownMenuItem onClick={() => onOpen("deleteServer", { server })} className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
                         Delete Classroom
                         <Trash className="h-4 w-4 ml-auto"/>
                     </DropdownMenuItem>
