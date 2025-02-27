b9401d770a3283658b544821017816e68e3fc6a1
Aayush Vaibhaw <127970657+aayushvbw@users.noreply.github.com>
2025-02-27 10:32:00 +0530

    done with page redirect for channels/members

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,8 +1,55 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
 
-const ServerIdPage = () => {
-    return (
-        <div>Server ID Page</div>
-    );
+import { RedirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ServerIdPageProps{
+    params: {
+        serverId: string,
+    }
+};
+
+const ServerIdPage = async ({
+    params
+}: ServerIdPageProps) => {
+    const profile = await currentProfile();
+
+    if(!profile){
+        return RedirectToSignIn;
+    }
+
+    const awaitedParam = await params;
+    const serverId = awaitedParam.serverId;
+
+    const server = await db.server.findUnique({
+        where: {
+            id: serverId,
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        },
+        include: {
+            channels: {
+                where: {
+                    name: "general"
+                },
+                orderBy: {
+                    createdAt: "asc"
+                }
+            }
+        }
+    });
+
+    const initialChannel = server?.channels[0];
+
+    if(initialChannel?.name !== "general"){
+        return null;
+    }
+
+    return redirect(`/servers/${serverId}/channels/${initialChannel?.id}`);
 }
 
 export default ServerIdPage;
