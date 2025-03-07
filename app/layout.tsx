7b72fda2ed3579ee8cdc16b12c02a2fc34cd1b60
Aayush Vaibhaw <127970657+aayushvbw@users.noreply.github.com>
2025-03-07 15:32:12 +0530

    chat messages done now to add api

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { SocketProvider } from "@/components/providers/socket-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 import { cn } from "@/lib/utils";
 
@@ -35,7 +36,9 @@ export default function RootLayout({
           <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} storageKey="dark-mode">
             <SocketProvider>
               <ModalProvider/>
-              {children}
+              <QueryProvider>
+                {children}
+              </QueryProvider>
             </SocketProvider>
           </ThemeProvider>
         </body>
